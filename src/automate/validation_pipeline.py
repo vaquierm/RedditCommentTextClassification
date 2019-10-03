941fbab92c36a987bd758ab88621c3420e174a90
Michael Vaquier <michaelvaquier74@gmail.com>
2019-10-03 17:44:49 -0400

    Can now convert all raw data comments to feature vectors (#15)
    * creation of Dictionary done with a few utility functions
    
    * Utilis done to load all the raw data.
    
    * Dictionary class done to convert a set of comments to a feature matrix based on a vocabulary
    
    * Automated process of converting comments to feature vectors done
    
    * Added exceptions for files not present
    
    * Done the automation outline of the project

--- src/automate/validation_pipeline.py
+++ src/automate/validation_pipeline.py
@@ -1,15 +1,15 @@
+from src.main import vocabularies_to_run, models_to_run
+
 # This file contains all automation for validating the different models against different vocabularies
 
 
-def main(processed_dir_path: str = "../../data/processed_data", vocabularies_dir_path: str = "../../data/vocabularies", results_dir_path: str = "../../results"):
-    vocabularies_to_run = ["LEMMATIZED", "CORRELATED"]
-    models_to_run = ["LR", "NB", "DT", "SVM", "SUPER"]
+def run_validation_pipeline():
     # TODO: Implement the validation pipeline
-    # Load all the specified vocabularies in Dictionaries, run each model on each dictionary.
+    # Load all the specified vocabularies in Dictionaries, run each model on each dictionary. (The lists of things to run are imported at the top)
     # Generate graphs and txt files for results in the results folder.
     # https://github.com/vaquierm/RedditCommentTextClassification/issues/6
     raise Exception("Not implemented yet")
 
 
 if __name__ == "__main__":
-    main()
+    run_validation_pipeline()
--- src/data_processing/dictionary.py
+++ src/data_processing/dictionary.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from src.utils.utils import csv_to_python_list
+
+
 # This file contains all abstraction dealing with the conversion from reddit comment to feature vector based on a specific vobabulary
 
 
@@ -8,16 +13,32 @@ class Dictionary:
         Creates a Dictionary based on a vocabulary
         :param vocabulary_file_path: File path of the vocabulary to base this dictionary on
         """
-        raise Exception("Not implemented yet")
-        # TODO: Michael implement this
-        # https://github.com/vaquierm/RedditCommentTextClassification/issues/5
 
-    def comments_to_feature_vector(self, comments: list):
+        # Load the vocabulary from the folder
+        vocabulary = csv_to_python_list(vocabulary_file_path)
+
+        # Create the mapping from word to index
+        # word: str -> index_in_feature_vector: int
+        self.word_map = {}
+        for i in range(len(vocabulary)):
+            self.word_map[vocabulary[i][0]] = i
+
+        # Save the length that the feature vectors will have
+        self.M = len(self.word_map.keys())
+
+    def comments_to_feature_vector(self, comments: list, frequency: bool = False):
         """
         Converts the list of comments to a feature matrix
         :param comments: List of string containing all comments to be converted
-        :return: X (NxM) where N is the number of comments and M is the number of wods in our dictionary
+        :param frequency: If True , count the frequency of each word for the feature vector. Otherwise just indicate the presence or absence with 1 and 0
+        :return: X (NxM) where N is the number of comments and M is the number of words in our dictionary
         """
-        raise Exception("Not implemented yet")
-        # TODO: Michael implement this
-        # https://github.com/vaquierm/RedditCommentTextClassification/issues/10
+        X = np.zeros((len(comments), self.M))
+
+        # For each comment, go through every word and create a feature vector
+        for i in range(len(comments)):
+            words = comments[i].split(' ')
+            for word in words:
+                if word in self.word_map:
+                    X[i][self.word_map[word]] = 1
+        return X
