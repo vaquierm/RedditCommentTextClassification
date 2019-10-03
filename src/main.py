eddf5f3d076deeb146cea488299c0f28d911b831
vaquierm <michaelvaquier74@gmail.com>
2019-10-03 16:48:07 -0400

    Done the automation outline of the project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import os
 
+from src.automate.create_feature_vectors import convert_all_raw_data_to_feature_vectors
+from src.automate.create_vocabularies import create_vocabularies
+from src.automate.validation_pipeline import run_validation_pipeline
+
 # Contains the raw data downloaded from https://www.kaggle.com/c/reddit-comment-classification-comp-551/data
 raw_data_dir_path: str = "../../data/raw_data"
 # Contains all the data in feature vector form
@@ -26,6 +30,18 @@ def main():
         raise Exception("The vocabularies folder does not exist")
     if not os.path.isdir(results_dir_path):
         raise Exception("The results folder does not exist")
+
+    # First create all the specified vocabularies from the original raw data
+    # Create copies of the original raw data by taking into account the lemmatization, feature selection etc
+    create_vocabularies()
+
+    # Once the vocabularies are created, convert the raw data to feature vectors and save them into csv format
+    convert_all_raw_data_to_feature_vectors()
+
+    # Once the feature vectors all converted to csv format, load them
+    # then run a k-fold validation against each one of them
+    # All results produced here will be found in the results folder
+    run_validation_pipeline()
 
 
 if __name__ == '__main__':
