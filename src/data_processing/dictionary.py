4403733a4569e3f655c3ccde6853b57f33476266
vaquierm <michaelvaquier74@gmail.com>
2019-10-03 12:58:36 -0400

    creation of Dictionary done with a few utility functions

--- src/data_processing/dictionary.py
+++ src/data_processing/dictionary.py
@@ -1,23 +1,36 @@
+from src.utils.utils import csv_to_python_list
+
+
 # This file contains all abstraction dealing with the conversion from reddit comment to feature vector based on a specific vobabulary
 
 
 class Dictionary:
 
-    def __init__(self, vocabulary_file_path: str):
+    def __init__(self, vocabulary_file_path: str, vocabulary_name: str):
         """
         Creates a Dictionary based on a vocabulary
         :param vocabulary_file_path: File path of the vocabulary to base this dictionary on
+        :param vocabulary_name: Name of the vocabulary being loaded
         """
-        raise Exception("Not implemented yet")
-        # TODO: Michael implement this
-        # https://github.com/vaquierm/RedditCommentTextClassification/issues/5
+
+        self.vocabulary_name = vocabulary_name
+
+        # Load the vocabulary from the folder
+        vocabulary = csv_to_python_list(vocabulary_file_path)
+
+        # Create the mapping from word to index
+        # word: str -> index_in_feature_vector: int
+        self.word_map = {}
+        for i in range(len(vocabulary)):
+            self.word_map[vocabulary[i]] = i
+
+        # Save the length that the feature vectors will have
+        self.M = len(self.word_map.keys())
 
     def comments_to_feature_vector(self, comments: list):
         """
         Converts the list of comments to a feature matrix
         :param comments: List of string containing all comments to be converted
-        :return: X (NxM) where N is the number of comments and M is the number of wods in our dictionary
+        :return: X (NxM) where N is the number of comments and M is the number of words in our dictionary
         """
-        raise Exception("Not implemented yet")
-        # TODO: Michael implement this
-        # https://github.com/vaquierm/RedditCommentTextClassification/issues/10
+        # TODO
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,1 +1,49 @@
+import csv
+import pandas as pd
+import numpy as np
+
+
 # This file contains any util functions needed across the project.
+
+def csv_to_python_list(file_path: str):
+    """
+    Reads a csv file and puts it into a python list
+    :param file_path: File path of the csv file
+    :return: Python list corresponding to the csv
+    """
+    with open(file_path, 'rb') as f:
+        reader = csv.reader(f)
+        list_data = list(reader)
+    return list_data
+
+
+def python_list_to_csv(file_path: str, list_data: list):
+    """
+    Writes data from a python list to a csv file
+    :param file_path: File path to write the data to
+    """
+    with open(file_path, 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(list_data)
+
+
+def load_raw_training_data(file_path: str, convert_subreddits_to_number: bool = True):
+    """
+    Loads the raw training data from the csv file
+    :param file_path: File path of raw data
+    :param convert_subreddits_to_number: If true, converts all subreddits to a number corresponding to its class
+    :return: List of comments, (List of associated subreddits or numpy array of numbers corresponding to the subreddits)
+    """
+    df = pd.read_csv(file_path)
+    return list(df['comments']), list(df['subreddits'])
+
+
+def load_raw_test_data(file_path: str):
+    """
+    Loads the raw data test from the csv file
+    :param file_path: File path of raw data
+    :return: numpy array (Nx1) of id's of comments, List of comments
+    """
+    df = pd.read_csv(file_path)
+    ids = list(df['id'])
+    return np.array(ids).reshape((len(ids), 1)), list(df['comments'])
