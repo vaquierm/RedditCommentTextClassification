cfb1789a495fd4d6f2125a2dc44555011d0464d2
vaquierm <michaelvaquier74@gmail.com>
2019-10-03 21:27:14 -0400

    Ensemble blending classifier written

--- src/models/SuperModel.py
+++ src/models/SuperModel.py
@@ -1,7 +1,9 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
 
 
 from src.models.Model import Model
@@ -15,13 +17,11 @@ class SuperModel(Model):
         # Create all the models that will be used in the ensemble # TODO: Figure out what models are good here
         self.models = []
         self.models.append(SVC(kernel='linear', decision_function_shape='ovr', class_weight='balanced'))
-        self.models.append(SVC(kernel='rbf', decision_function_shape='ovr', class_weight='balanced'))
-        self.models.append(
-            RandomForestClassifier(n_estimators=150, max_depth=10, random_state=0, class_weight='balanced'))
-        self.models.append(
-            RandomForestClassifier(n_estimators=150, max_depth=10, random_state=0, class_weight='balanced'))
+        self.models.append(RandomForestClassifier(n_estimators=150, max_depth=10, random_state=0, class_weight='balanced'))
+        self.models.append(GaussianNB())
+        self.models.append(KNeighborsClassifier())
 
-        self.meta_model = LogisticRegression()
+        self.meta_model = SVC(kernel='linear', decision_function_shape='ovr', class_weight='balanced')
 
     def fit(self, X, Y):
         """
@@ -32,9 +32,20 @@ class SuperModel(Model):
         """
         super().fit(X, Y)
 
+        self.M = X.shape[1]
+
+        # Split the data
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+
         # Fit the data to each classifier in our ensemble model
         for model in self.models:
-            model.fit(X, Y)
+            model.fit(X_train, Y_train)
+
+        # Now we predict the outcomes
+        predictions = self.predict_all_classifiers(X_test)
+
+        # Train the meta classifier with the predictions
+        self.meta_model.fit(predictions, Y_test)
 
     def predict(self, X):
         """
@@ -44,6 +55,18 @@ class SuperModel(Model):
         """
         super().predict(X)
 
+        # Predict the outcomes with all classifiers
+        predictions = self.predict_all_classifiers(X)
+
+        # Predict with the meta classifier
+        return self.meta_model.predict(predictions)
+
+    def predict_all_classifiers(self, X):
+        """
+        Predict the outcome with all classifiers
+        :param X: X
+        :return: (NxC) All outcomes for all classifiers
+        """
         N = X.shape[0]
 
         predictions = np.empty((N, len(self.models)))
@@ -51,10 +74,4 @@ class SuperModel(Model):
         for i in range(len(self.models)):
             predictions[:, i] = self.models[i].predict(X)
 
-        voted_predictions = np.empty(N)
-        # Collect the votes of each classifiers
-        for i in range(N):
-            unique, counts = np.unique(predictions[i], return_counts=True)
-            voted_predictions[i] = unique[np.where(counts == counts.max())[0][0]]
-
-        return voted_predictions
+        return predictions
