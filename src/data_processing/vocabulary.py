1788777f2c697ca8e65b11cfbf18a717a24e5b94
Marine Huynh <marine.huynh@yahoo.ca>
2019-10-06 20:32:01 -0400

    vocabulary for small dataset

--- src/data_processing/vocabulary.py
+++ src/data_processing/vocabulary.py
@@ -1,13 +1,15 @@
 # This file contains all functions to related to creating a clean vocabulary
 import os
 import nltk
-import numpy as np
-from sklearn.datasets import fetch_20newsgroups
+import re
+import emoji
 from src.utils.utils import load_raw_training_data
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
+from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from emoji import UNICODE_EMOJI
 
 # look this up for noo, noooooo, nooooooooo
 from nltk.tokenize import TweetTokenizer
@@ -17,9 +19,10 @@ from nltk.tokenize import TweetTokenizer
 def main():
     create_vocab()
 
-
 def create_vocab():
+
     # two different lists of stopwords and nltk.corpus stopwords
+    # list of stopwordList is larger than nltk.stopwords but does it even matter due to lemmatization
     # common list of stop words taken from https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/feature_extraction/stop_words.py
     stopwordList = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all",
                        "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst",
@@ -54,117 +57,45 @@ def create_vocab():
                        "yours", "yourself", "yourselves"]
 
     wordsLem = stopwords.words('english')
-    # pass in all the criteria necessary
-    vectorizer = CountVectorizer(stop_words=wordsLem, ngram_range=(1, 2), strip_accents='ascii')
-    print(vectorizer)
 
+    # pass in all the criteria necessary
+    vectorizer = CountVectorizer(stop_words=stopwordList, ngram_range=(1, 2), strip_accents='ascii')
 
     raw_data_dir_path: str = "../../data/raw_data"
     train_raw_data_path = os.path.join(raw_data_dir_path, "reddit_train.csv")
     # Load the raw dataset
     comments_train, Y_train = load_raw_training_data(train_raw_data_path, convert_subreddits_to_number=False)
 
-    comments_test = ["https://youtu.be/6xxbBR8iSZ0?t=40m49s\n\nIf you didn't find it already.\n\nNothing out of the ordinary though, she just has eye constant eye contact.https://www.youtube.com/watch?v=L9Hlj2bawFI","The striped bats are hanging on their feet for best fishes","oranges are good! :) :') <3", "AHHH, this is soooo coooool", "The dog is running, the cat ran, and the pig runs."]
     # test comments because the list is too long.
-    # comments_test = []
-    # comments_test.append(comments_train[2])
-    print("these are the comments")
-    print(comments_test)
+    comments_test = ["lmaooooooooo :) https://youtu.be/6xxbBR8iSZ0?t=40m49s\n\n I loooooove you. If you didn't find it already. connection, connected connecting, cont. \n\nNothing out of the ordinary though, she just has eye constant eye contact. (https://www.reddit.com/r/music/wiki/halloffame)","The striped bats are hanging on their feet for best fishes","oranges are good! :) :') <3 https://www.youtube.com/watch?v=L9Hlj2bawFI", "AHHH, this is soooo coooooooooooooooool", "The dog is running, the cat ran, and the pig runs."]
 
-    print("--------find_urls----------")
-    url, urls_matrix = find_urls(comments_test)
-    print(url)
-    print(urls_matrix)
-
-    print("----tweet_elements----------")
-    tweet_element = tweet_elements(comments_test)
-    print(tweet_element)
-
-    print("----get_new_inputs_comments----------")
+    #preprocess the dataset
+    comments_test = replace_all_for_strong_vocab(comments_test)
     inputs = get_new_input_comments(comments_test)
-    print("new inputs from lemmatization")
-    print(inputs)
-
-    #todo need to find a way to tokenize, OR merge matrices for each Lemmatization, urls, smiley, and coooool. at the end.
-    #inputs = ['allo', 'bien', 'bonjour', 'https://youtube.com/ahsss.eee', 'bien']
-    #cannot have inputs = [['allo', 'bien'], ['bonjour', 'https://youtube.com/ahsss.eee', 'bien']]
+    print("inputs: ", inputs)
 
     X = vectorizer.fit_transform(inputs)
     vocab = vectorizer.get_feature_names()
-    # add manually a word URL but needs work to be part of the matrix. (maybe simply a counter per comments)
-    vocab.append("URL")
     print(vocab)
     print(X.toarray())
 
-## end of the important code. If we want other "features/constraints" add them to the new input.
 
-# to remove ----------------
-    # # todo need to have this as the original inputs. to be able to pass it to fit_transform
-    # print("doing lemmatization below: is simply for plurial..., and returns a list")
-    # lemmatizer = WordNetLemmatizer()
-    # lemmatized_outputs = ' '.join([lemmatizer.lemmatize(w) for w in vectorizer.get_feature_names()])
-    # # lemmatized_outputs is a sentence that can be appended back into the input
-    # vocab2 = lemmatized_outputs.split(" ")
-    # print("the list of vocabulary with repeating words")
-    # print(vocab2)
-    # vocab_list = []
-    # # list of vocabulary that does not have repeated words.
-    # for i in range(len(vocab2)):
-    #     if vocab2[i] not in vocab_list:
-    #         vocab_list.append(vocab[i])
-    # print("The list of vocabulary without repeating words")
-    # print(vocab_list)
-# ---------------------------
-
-def tweet_elements(comments):
-    tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
-    for element in range(len(comments)):
-        tweetValues = tknzr.tokenize(comments[element])
-        print(tweetValues)
-    return tweetValues
-
-# method that returns both the list of all the urls and the matrix for if any url was present
-def find_urls(comments):
-    urls = []
-    sizeOfMatrix = len(comments) *2
-    urlMatrix = np.arange(sizeOfMatrix).reshape(len(comments), 2)
-
-    for element in range(len(comments)):
-        #initialize the matrix and the counter
-        count = 0
-        urlMatrix[element, 0] = element
-        urlMatrix[element, 1] = 0
-
-        # tokenize in a way to keep the whole url together.
-        sentence = comments[element].split()
-        for w in sentence:
-            if "https://" in w:
-                count += 1
-                urls.append(w)
-                urlMatrix[element, 1] = count
-
-    return urls, urlMatrix
-
-
-# takes in parameters the list of comments ["comment1", "comment2"]
+# This method must be called after preprocessing the data input as it will tokenize by words.
 def get_new_input_comments(comments):
+
     lemmatized_inputs = []
     lemmatizer = WordNetLemmatizer()
     for i in range(len(comments)):
         sentence = []
-        #todo tokenize in a way that the urls are kept together... or work with the method find_urls
-        #Todo if already pass as words, no need for tokenization here.
-        # maybe even call the link inside here.
         for w in nltk.word_tokenize(comments[i]):
-        #for w in comments[element].split():
 
             word = lemmatizer.lemmatize(w, get_wordnet_pos(w))
+            word = reduce_lengthening(word)
             sentence.append(word)
 
         new_sentence = ' '.join(sentence)
         lemmatized_inputs.append(new_sentence)
     return lemmatized_inputs
-
 
 def get_wordnet_pos(word):
     """Map POS tag to first character lemmatize() accepts"""
@@ -175,6 +106,60 @@ def get_wordnet_pos(word):
                 "R": wordnet.ADV}
 
     return tag_dict.get(tag, wordnet.NOUN)
+
+def reduce_lengthening(text):
+    """
+    Replace repeated character sequences of length 3 or greater with sequences
+    of length 2.
+    """
+    pattern = re.compile(r"(.)\1{2,}")
+    return pattern.sub(r"\1\1", text)
+
+def replace_all_for_strong_vocab(comments):
+    for i in range(len(comments)):
+        comments[i] = replace_youtube_links(comments[i])
+        comments[i] = replace_url(comments[i])
+        comments[i] = replace_smiley(comments[i])
+    return comments
+
+def replace_youtube_links(sentence):
+    youtube_regex = ( r'(https?://)?(www\.)?' '(youtube|youtu|youtube-nocookie)\.(com|be|ca)/' '(watch\?.*?(?=v=)v=|embed/|v/|.+\?v=)?([^&=%\?]{11})' '(\?t=((\d+)h)?((\d{2})m)?((\d{2})s)?)?')
+    return re.sub(youtube_regex, 'youtubelink  ', sentence)
+
+def replace_url(sentence):
+    regex = (r'(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$')
+    return re.sub(regex, 'internetlink ', sentence)
+
+def replace_smiley(sentence):
+
+    emoticonFunny = [':)', ':-)', ":')", ':P', ':D', ":'-)"]
+    str = ""
+
+    tknzr = TweetTokenizer(strip_handles=False, reduce_len=True, preserve_case=False)
+    commentTokenized = tknzr.tokenize(sentence)
+    # stemming the dataset here
+    commentTokenized = stemming(commentTokenized)
+
+    for index_word in range(len(commentTokenized)):
+        for i in range(len(emoticonFunny)):
+            if commentTokenized[index_word] == emoticonFunny[i]:
+                commentTokenized[index_word] = "emoticonFunny"
+
+        str = str + ' ' + commentTokenized[index_word]
+    sentence = str
+
+    return sentence
+
+# pass in a list of words and return a list of words with the words stemmed.
+def stemming(commentTokenized):
+
+    list = []
+    ps = PorterStemmer()
+
+    for word in commentTokenized:
+        list.append(ps.stem(word))
+
+    return list
 
 
 if __name__ == "__main__":
