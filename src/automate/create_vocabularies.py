614ec067270977aa2988ec57585ec5d7c72dae53
Michael Vaquier <michaelvaquier74@gmail.com>
2019-10-07 13:12:58 -0400

    The vocabularies can now save to files automatically (#19)
    * Started cleanup of vocabulary creation
    
    * Automation of vocab creation to be tested
    
    * Automation works for vocab
    
    * Automation working but we probably dont wanna save the non sparse matrices

--- src/automate/create_vocabularies.py
+++ src/automate/create_vocabularies.py
@@ -5,39 +5,39 @@ from src.config import vocabularies_to_run, vocabularies_dir_path, raw_data_dir_
 from src.utils.utils import load_raw_test_data, load_raw_training_data
 from src.data_processing.vocabulary import create_vocab
 
-from src.utils.utils import python_list_to_csv
-
+from src.utils.utils import save_cleaned_raw_data
 
 
 # This file contains the automated script to create all the specified vocabularies
 
-
 def create_vocabularies():
     # Check if the original raw data files are present
-    train_raw_data_path = os.path.join(raw_data_dir_path, "reddit_train.csv")
-    test_raw_data_path = os.path.join(raw_data_dir_path, "reddit_test.csv")
+    train_raw_data_path = os.path.join("..", raw_data_dir_path, "reddit_train.csv")
+    test_raw_data_path = os.path.join("..", raw_data_dir_path, "reddit_test.csv")
     if not os.path.isfile(train_raw_data_path):
         raise Exception("The original raw training data cannot be found at " + train_raw_data_path)
     if not os.path.isfile(test_raw_data_path):
         raise Exception("The original raw testing data cannot be found at " + train_raw_data_path)
 
+    print("\n\nCreating vocabularies")
+
     # For a vocabulary for each type specified
     for vocabulary in vocabularies_to_run:
+
         # Load the raw dataset
         comments_train, Y_train = load_raw_training_data(train_raw_data_path, convert_subreddits_to_number=False)
         id_test, comments_test = load_raw_test_data(test_raw_data_path)
 
-        # Do some processing and stuff by calling helper functions in src/data/processing/vocabulary.py
+        print("\tCreating vocabulary: " + vocabulary)
 
-    # TODO: Marine implement the function to create the vocabulary
-    vocabulary, vocab_vectors = create_vocab(comments_train)
-    vocabulary_path = "../data/vocabularies/vocabulary.csv"
-    python_list_to_csv(vocabulary_path,  vocabulary)
+        # Create the vocabs
+        comments_train_clean, comments_test_clean = create_vocab(comments_train, comments_test, vocabulary)
 
-    # We want to load the training set, go through all the words, clean then (lemmatize etc...) and save the raw vocab to the vocabularies folder.
-    # https://github.com/vaquierm/RedditCommentTextClassification/issues/3
-    # MAKE SURE to write all your specific helper methods for this in src/data/processing/vocabulary.py
-    #raise Exception("Not yet Implemented")
+        # Save both cleaned comment lists to csv files
+        clean_raw_train_path = os.path.join("..", raw_data_dir_path, vocabulary + "_train_raw_clean.csv")
+        save_cleaned_raw_data(clean_raw_train_path, train_raw_data_path, comments_train_clean)
+        clean_raw_test_path = os.path.join("..", raw_data_dir_path, vocabulary + "_test_raw_clean.csv")
+        save_cleaned_raw_data(clean_raw_test_path, test_raw_data_path, comments_test_clean)
 
 
 if __name__ == '__main__':
