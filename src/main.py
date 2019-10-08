a4c274e00e869f7dc8f2669b9a0688596d7daad2
Michael Vaquier <michaelvaquier74@gmail.com>
2019-10-07 22:26:08 -0400

    Outputs generate automatically for kaggle (#25)
    * The super model runs with the pipeline
    
    * Automation done for kaggle outputs

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 
 from src.create_vocabularies import create_vocabularies
 from src.validation_pipeline import run_validation_pipeline
+from src.generate_kaggle_results import generate_kaggle_results
 from src.config import raw_data_dir_path,processed_dir_path,vocabularies_dir_path,results_dir_path
 
 
@@ -20,9 +21,12 @@ def main():
     # Create copies of the original raw data by taking into account the lemmatization, feature selection etc
     create_vocabularies()
 
-    # then run a k-fold validation against each one of them
+    # Run a k-fold validation against each one of them
     # All results produced here will be found in the results folder
     run_validation_pipeline()
+
+    # Run the specified best model to generate results to be submitted on Kaggle
+    generate_kaggle_results()
 
 
 if __name__ == '__main__':
